694bde47921195d7b6126fc5d467caed3f6e2be2
MohamedMahsoon-Nsaq <mahsoon@nsaq.io>
2024-11-27 16:53:19 +0200

    handle background logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import { useState } from 'react';
 import AppRoutes from './routes/AppRoutes';
 import { BrowserRouter } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 const App = () => {
   const isAuthenticated = true; //useSelector((state) => state.auth.isAuthenticated);
   return (
     <BrowserRouter>
-      <div className="ISP-App">
-        <div className="ISP-App-container">
-          <AppRoutes isAuthenticated={isAuthenticated} />
-        </div>
+      <div className="ISP-App-container">
+        <AppRoutes isAuthenticated={isAuthenticated} />
       </div>
     </BrowserRouter>
   );
